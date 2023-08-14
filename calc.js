8f3dee21ddb8caff42e7f33eb47ba4e90255c9eb
gopi5086 <142210015+gopi5086@users.noreply.github.com>
2023-08-14 01:22:57 -0700

    calc file upload

---
+++ calc.js
@@ -0,0 +1,20 @@
+const calculatormodule={
+    sum:function(num1,num2)
+    {
+        return num1+num2
+    },
+    sub:function(num1,num2)
+    {
+        return num1-num2
+    },
+    mul:function(num1,num2)
+    {
+        return num1*num2
+    },
+    div:function(num1,num2)
+    {
+        return num1/num2
+    },
+    
+}
+module.experts=calculatormodule;
